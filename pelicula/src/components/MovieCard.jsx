ea32dc3b0a16206c782d956480ee76f17f2910b6
Arianno Sanchez <ariannosanchezmix@outlook.com>
2022-12-23 19:05:41 -0500

    first commit

---
+++ pelicula/src/components/MovieCard.jsx
@@ -0,0 +1,19 @@
+import styles from "../components/MovieCard.module.css";
+import { Link } from "react-router-dom";
+export function MovieCard({ movie }) {
+  const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+  return (
+    <li className={styles.movieCard}>
+      <Link to={"/movies/" + movie.id}>
+        <img
+          className="movieImage"
+          width={230}
+          height={345}
+          src={imageUrl}
+          alt={movie.title}
+        />
+      </Link>
+      <div className={styles.tile}>{movie.title}</div>
+    </li>
+  );
+}
---
+++ pelicula/src/pages/MovieDetails.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get } from "../data/httpClient";
+import { getMovieImg } from "../utils/getMovieImg";
+import "../pages/MovieDetails.css"
+export function MovieDetails() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState([]);
+  const [generos, setGeneros] = useState([]);
+
+  useEffect(() => {
+    get("/movie/" + movieId).then((data) => {
+      setMovie(data);
+      setGeneros(data.genres[0]);
+      console.log(data);
+    });
+  }, [movieId]);
+  const imageUrl = getMovieImg(movie.poster_path, 500);
+
+  return (
+    <div className="detailsContainer">
+      <img className="col movieImage" src={imageUrl} alt={movie.title} />
+      <div className="col movieDetails">
+        <p className="title">
+          <strong>Título: </strong>
+          {movie.title}
+        </p>
+        <p>
+          <strong>Género: </strong>
+          {generos.name}
+        </p>
+        <p>
+          <strong>Descripción: </strong>
+          {movie.overview}
+        </p>
+      </div>
+    </div>
+  );
+}
